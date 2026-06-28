ec014acffbaef3b4f0a6e001b5445e329b04cd34
matheusmatos84 <matheusestuda8493@gmail.com>
2026-06-28 14:14:14 -0300

    Add files via upload

---
+++ gamespot-main/gamespot-main/footer.php
@@ -0,0 +1,41 @@
+<!-- FOOTER -->
+<footer class="footer text-center" style="padding: 45px 0;">
+    <div class="container">
+        <div class="row">
+
+            <!-- LOCALIZAÇÃO -->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Localização</h4>
+                <p class="lead mb-0">
+                    Campus IFPR
+                    <br>
+                    Telêmaco Borba, PR
+                </p>
+            </div>
+
+            <!-- REDES SOCIAIS -->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Perfil em Website</h4>
+
+                <div class="d-flex justify-content-center">
+                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                </div>
+            </div>
+
+            <!-- ANUNCIE -->
+            <div class="col-lg-4">
+                <h4 class="text-uppercase mb-4">Anuncie</h4>
+
+                <p class="lead mb-0">
+                    Produtos sem utilidade?
+                    <br>
+                    Anuncie em nosso site!
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
---
+++ gamespot-main/gamespot-main/masterhead.php
@@ -0,0 +1,21 @@
+<!-- TELA INICIAL -->
+    <header class="masthead bg-secondarytext-white text-center">
+    <div class="container d-flex align-items-center flex-column" style="margin-top: 10px; margin-bottom: 70px;">
+            <i class="bi bi-skip-start" style="font-size: 80px; margin-top: -40px;"></i>
+            <i class="bi bi-controller" style="font-size: 200px; margin-top: -25px;"></i> 
+            <h1 class="masthead-heading text-uppercase mb-2 text-center">Gamingspot</h1>
+            <h1 class="text-uppercase mt-1 text-center" style="font-size: 32px;">
+                Vendas & Anúncios
+            </h1>
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon d-flex justify-content-center align-items-center">
+                    <i class="fas fa-star"></i>
+                </div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <p class="masthead-subheading font-weight-light mb-0" style="margin-top: -60px; font-size: 20px;">
+                Videogames - Consoles - Periféricos
+            </p>
+        </div>
+    </header>
